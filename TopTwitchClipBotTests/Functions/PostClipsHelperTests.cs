42c05df0c02cab25e68eb92418f8dd428b58a91d
Kirk Horton <mosentok@gmail.com>
2019-03-11 13:24:38 -0500

    Replaced explicit args with It.IsAny for the types.

--- TopTwitchClipBotTests/Functions/PostClipsHelperTests.cs
+++ TopTwitchClipBotTests/Functions/PostClipsHelperTests.cs
@@ -78,7 +78,7 @@ namespace TopTwitchClipBotTests.Functions
             var channel = new Mock<IMessageChannel>();
             var insertedContainer = new InsertedBroadcasterHistoryContainer { ClipUrl = clipUrl, Title = title, Views = views, Duration = duration, CreatedAt = createdAt };
             var userMessage = new Mock<IUserMessage>();
-            channel.Setup(s => s.SendMessageAsync(expectedMessage, false, null, null)).ReturnsAsync(userMessage.Object);
+            channel.Setup(s => s.SendMessageAsync(expectedMessage, It.IsAny<bool>(), It.IsAny<Embed>(), It.IsAny<RequestOptions>())).ReturnsAsync(userMessage.Object);
             var inserted = new List<InsertedBroadcasterHistoryContainer> { insertedContainer };
             var channelContainer = new ChannelContainer(inserted, channel.Object);
             var task = _Helper.SendMessagesAsync(channelContainer);
