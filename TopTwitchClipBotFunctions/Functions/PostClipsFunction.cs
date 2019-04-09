2e3e580ca99b46542a5e135779f253d738c77b32
Kirk Horton <mosentok@gmail.com>
2019-04-09 18:43:10 -0500

    Need to read local settings json file.

--- TopTwitchClipBotFunctions/Functions/PostClipsFunction.cs
+++ TopTwitchClipBotFunctions/Functions/PostClipsFunction.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -18,6 +19,8 @@ namespace TopTwitchClipBotFunctions.Functions
         public static async Task Run([TimerTrigger("%PostClipsFunctionCron%")]TimerInfo myTimer, ILogger log)
         {
             var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", true)
                 .AddEnvironmentVariables()
                 .Build();
             var configWrapper = new ConfigurationWrapper(config);
